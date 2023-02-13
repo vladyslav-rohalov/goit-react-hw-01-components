07c61c8d5918ef88f34ce2485bff2a63cd5209c2
Vladyslav Rohalov <domus.lg@gmail.com>
2023-02-13 15:12:26 +0100

    Edited FriendsList

--- src/components/Friends/FriendsList.jsx
+++ src/components/Friends/FriendsList.jsx
@@ -4,7 +4,17 @@ import FriendsListItem from './FriendsListItem';
 export default function FriendsList({ friends }) {
   return (
     <ul className={css.friendList}>
-      <FriendsListItem friends={friends} />
+      {friends.map(friend => {
+        const { id, isOnline, avatar, name } = friend;
+        return (
+          <FriendsListItem
+            key={id}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+          />
+        );
+      })}
     </ul>
   );
 }
--- src/components/Friends/FriendsListItem.jsx
+++ src/components/Friends/FriendsListItem.jsx
@@ -1,35 +1,24 @@
 import PropTypes from 'prop-types';
 import css from './FriendsListItem.module.css';
 
-export default function FriendsListItem({ friends }) {
-  return friends.map(painting => {
-    return (
-      <li className={css.item} key={painting.id}>
-        {painting.isOnline ? (
-          <span className={`${css.status} ${css.isOnline}`}></span>
-        ) : (
-          <span className={`${css.status} ${css.isOffline}`}></span>
-        )}
+export default function FriendsListItem({ id, isOnline, avatar, name }) {
+  return (
+    <li className={css.item} key={id}>
+      {isOnline ? (
+        <span className={`${css.status} ${css.isOnline}`}></span>
+      ) : (
+        <span className={`${css.status} ${css.isOffline}`}></span>
+      )}
 
-        <img
-          className={css.avatar}
-          src={painting.avatar}
-          alt="User avatar"
-          width="48"
-        />
-        <p className={css.name}>{painting.name}</p>
-      </li>
-    );
-  });
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
 }
 
 FriendsListItem.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.exact({
-      avatar: PropTypes.string,
-      name: PropTypes.string,
-      isOnline: PropTypes.bool,
-      id: PropTypes.number,
-    })
-  ),
+  id: PropTypes.number,
+  isOnline: PropTypes.bool,
+  avatar: PropTypes.string,
+  name: PropTypes.string,
 };
